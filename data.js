774fe547a87699bb367505db1cae072cc24f201e
giabaovolong15-oss <giabaovolong15@gmail.com>
2026-06-30 14:05:00 +0700

    data.js

--- data.js
+++ data.js
@@ -3,49 +3,49 @@ const vtuberList = [
         id: "ymk-0001",
         name: "Yumika",
         tags: ["B-Galaxy"],
-        image: "images/Z.A.Italic/01-Yumika.png",
+        image: "images/01-Yumika.png",
         lastUpdate: "01:13, 29/06/2026"
     },
     {
         id: "ymk-0002",
         name: "Miyuki",
         tags: ["Mì"],
-        image: "images/Z.A.Italic/02-Miyuki.png",
+        image: "images/02-Miyuki.png",
         lastUpdate: "01:13, 29/06/2026"
     },
     {
         id: "ymk-0003",
         name: "Hana",
         tags: ["Hana"],
-        image: "images/Z.A.Italic/03-Hana.png",
+        image: "images/03-Hana.png",
         lastUpdate: "01:13, 29/06/2026"
     },
     {
         id: "ymk-0004",
         name: "Kurumizawa",
         tags: ["Kuru"],
-        image: "images/Z.A.Italic/04-Kurumizawa.png",
+        image: "images/04-Kurumizawa.png",
         lastUpdate: "01:13, 29/06/2026"
     },
     {
         id: "ymk-0005",
         name: "Little Yiyi",
         tags: ["Yi"],
-        image: "images/Z.A.Italic/05-Little Yiyi.png",
+        image: "images/05-Little Yiyi.png",
         lastUpdate: "01:13, 29/06/2026"
     },
     {
         id: "ymk-0006",
         name: "Joy",
         tags: ["Joy"],
-        image: "images/Z.A.Italic/06-Joy.png",
+        image: "images/06-Joy.png",
         lastUpdate: "01:13, 29/06/2026"
     },
     {
         id: "ymk-0007",
         name: "Elvinia Ivosta",
         tags: ["Ivosta"],
-        image: "images/Z.A.Italic/07-Elvinia Ivosta.png",
+        image: "images/07-Elvinia Ivosta.png",
         lastUpdate: "01:13, 29/06/2026"
     }
 ];
