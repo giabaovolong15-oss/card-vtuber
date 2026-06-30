22ed687557131ca6933433daf27b042c780f9d63
giabaovolong15-oss <giabaovolong15@gmail.com>
2026-06-30 13:23:26 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,51 @@
+const vtuberList = [
+    {
+        id: "ymk-0001",
+        name: "Yumika",
+        tags: ["B-Galaxy"],
+        image: "images/Z.A.Italic/01-Yumika.png",
+        lastUpdate: "01:13, 29/06/2026"
+    },
+    {
+        id: "ymk-0002",
+        name: "Miyuki",
+        tags: ["Mì"],
+        image: "images/Z.A.Italic/02-Miyuki.png",
+        lastUpdate: "01:13, 29/06/2026"
+    },
+    {
+        id: "ymk-0003",
+        name: "Hana",
+        tags: ["Hana"],
+        image: "images/Z.A.Italic/03-Hana.png",
+        lastUpdate: "01:13, 29/06/2026"
+    },
+    {
+        id: "ymk-0004",
+        name: "Kurumizawa",
+        tags: ["Kuru"],
+        image: "images/Z.A.Italic/04-Kurumizawa.png",
+        lastUpdate: "01:13, 29/06/2026"
+    },
+    {
+        id: "ymk-0005",
+        name: "Little Yiyi",
+        tags: ["Yi"],
+        image: "images/Z.A.Italic/05-Little Yiyi.png",
+        lastUpdate: "01:13, 29/06/2026"
+    },
+    {
+        id: "ymk-0006",
+        name: "Joy",
+        tags: ["Joy"],
+        image: "images/Z.A.Italic/06-Joy.png",
+        lastUpdate: "01:13, 29/06/2026"
+    },
+    {
+        id: "ymk-0007",
+        name: "Elvinia Ivosta",
+        tags: ["Ivosta"],
+        image: "images/Z.A.Italic/07-Elvinia Ivosta.png",
+        lastUpdate: "01:13, 29/06/2026"
+    }
+];
